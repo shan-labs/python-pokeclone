40d1fb3f56985e7426d8769e1d71f229541464f5
Khinshan Khan <khanshan501@gmail.com>
2022-02-25 17:21:30 -0500

    simple player class

--- pokeclone/__main__.py
+++ pokeclone/__main__.py
@@ -1,2 +1,9 @@
+from pokeclone.archetypes import characters
+
 if __name__ == '__main__':
-    print("hello")
+    print("Welcome to PokeClone!")
+    name = input("Please enter trainer name:")
+    gender = input("Please enter trainer gender:")
+    nature = input("Please enter trainer nature:")
+    p = characters.Player(name, gender, nature)
+    print(p)
---
+++ pokeclone/archetypes/characters.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self, name, gender, nature):
+        self.name = name
+        self.gender = gender
+        self.nature = nature
+        # self.pokelist = [starter]
+        self.money = 100
+        self.bag = {}
